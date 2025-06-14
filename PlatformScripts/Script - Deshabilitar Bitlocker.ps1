cdc2ae9b55b88eb26c7ffa2886d65e0ed37a56f7
alexsf93 <alexsf93@gmail.com>
2025-06-15 00:37:59 +0200

    Update Script - Deshabilitar Bitlocker.ps1

--- PlatformScripts/Script - Deshabilitar Bitlocker.ps1
+++ PlatformScripts/Script - Deshabilitar Bitlocker.ps1
@@ -1,2 +1,12 @@
+# README:
+# Antes de ejecutar este script, **asegúrate de que los equipos estén añadidos previamente a un grupo de exclusión en Intune**,
+# la consola de administración utilizada para la gestión de políticas de BitLocker.
+# Así evitarás que se apliquen las políticas de cifrado de BitLocker a estos dispositivos mientras realizas los cambios.
+# Si no haces este paso, existe el riesgo de que BitLocker vuelva a activarse o se apliquen políticas de cifrado automáticamente.
+
+# Instrucciones:
+# 1. Añade los equipos a un grupo de exclusión de BitLocker en Intune.
+# 2. Verifica que la exclusión se haya aplicado correctamente a todos los dispositivos.
+# 3. Ejecuta el siguiente script para desactivar BitLocker:
 $BLV = Get-BitLockerVolume
 Disable-BitLocker -MountPoint $BLV
